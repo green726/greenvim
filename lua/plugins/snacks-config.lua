e8bd582a024b7bd78431a9b59837fc7ba5b2d47e
Milan Lustig <milan@opt32.com>
2026-05-17 22:43:00 -0400

    dashboard

--- lua/plugins/snacks-config.lua
+++ lua/plugins/snacks-config.lua
@@ -3,6 +3,17 @@ return {
         enabled = true,
         preset = {
             header = require("random-headers"),
+            keys = {
+                { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                { icon = "󰍔 ", key = "m", desc = "Markdown Library", action = ":MarkdownLibrary" },
+                { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+            },
         },
         sections = {
             { section = "header", },
